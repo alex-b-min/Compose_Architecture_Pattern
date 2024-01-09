a8da7c7ed634f4191bb360bfef4b231a0f71bd8c
alex-b-min <alex.b.min@selvas.com>
2024-01-09 13:48:39 +0900

    chore: Room 의존성 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,11 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:2.45")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.3.0")
 }
