8a58ffce1c827dc1a00d30b1f747a3f29ed3067c
alex-b-min <alex.b.min@selvas.com>
2024-01-09 16:39:03 +0900

    feat: 노트를 추가하는 AddNoteUseCase 구현

--- app/src/main/java/com/example/compose_archtiecture_pattern/domain/model/Note.kt
+++ app/src/main/java/com/example/compose_archtiecture_pattern/domain/model/Note.kt
@@ -20,3 +20,5 @@ class Note(
         val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
     }
 }
+
+class InvalidNoteException(message: String): Exception(message)
--- app/src/main/java/com/example/compose_archtiecture_pattern/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/example/compose_archtiecture_pattern/domain/use_case/NoteUseCases.kt
@@ -2,5 +2,6 @@ package com.example.compose_archtiecture_pattern.domain.use_case
 
 data class NoteUseCases(
     val getNotes: GetNotesUseCase,
-    val deleteNotesUseCase: DeleteNotesUseCase
+    val deleteNotesUseCase: DeleteNotesUseCase,
+    val addNoteUseCase: AddNoteUseCase
 )
