fa5e0ce58ab5a3cf322ce7a18109288dcae3a2e3
alex-b-min <alex.b.min@selvas.com>
2024-01-09 13:28:16 +0900

    init: 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Compose_Archtiecture_Pattern"
+include(":app")
+ 
