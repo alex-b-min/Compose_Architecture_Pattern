327c45a2ffe0ba2fc186706387788f7fe9b7fa7e
alex-b-min <alex.b.min@selvas.com>
2024-01-10 09:31:07 +0900

    chore: flow 타입으로 상태 관리를 하기 위한 compose lifecycle 의존성 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.1")
     implementation("androidx.compose.foundation:foundation-android:1.5.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
